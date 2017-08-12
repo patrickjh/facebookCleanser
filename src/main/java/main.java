e16ee0418f5ca8f73c1ea6eae7e9f06652220c40
patrickjh <patrick@lessonswithpatrick.com>
2017-08-11 20:59:47 -0700

    got hello world set up - opesn up facebook from main method

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,23 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.net.URL;
+
 public class main {
 
     public static void main(String[] args) {
-        //started my app
+        System.out.println("testing");
+
+
+        System.setProperty("webdriver.gecko.driver",
+                "C:"+ File.separator + "Users" + File.separator +"patrick"
+                        + File.separator + "Desktop" + File.separator + "geckodriver.exe" );
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.facebook.com");
     }
 }
