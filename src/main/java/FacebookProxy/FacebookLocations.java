c526aeed0e2ac97135283794af96c5721d10da88
patrickjh <patrick@lessonswithpatrick.com>
2017-08-14 23:52:23 -0700

    changed login() to go to facebook.com/login.php

--- src/main/java/FacebookProxy/FacebookLocations.java
+++ src/main/java/FacebookProxy/FacebookLocations.java
@@ -5,13 +5,13 @@ class FacebookLocations {
     private FacebookLocations() {}
 
     static class LoginPage {
-        static final String URL = "http://www.facebook.com";
+        static final String URL = "http://www.facebook.com/login.php";
 
-        static final String EMAIL_ADDRESS_FIELD_XPATH = "//input[@data-testid='royal_email']";
+        static final String EMAIL_ADDRESS_FIELD_XPATH = "//input[@id='email']";
 
-        static final String PASSWORD_FIELD_XPATH = "//input[@data-testid='royal_pass']";
+        static final String PASSWORD_FIELD_XPATH = "//input[@id='pass']";
 
-        static final String LOGIN_BUTTON_XPATH = "//input[@data-testid='royal_login_button']";
+        static final String LOGIN_BUTTON_XPATH = "//button[@id='loginbutton']";
     }
 
     static class HomePage {
